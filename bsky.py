5e551d58eca35451e477d14b7b17f13f7e18a2ea
Diego Ramos Ruggeri <diego@ruggeri.net.br>
2025-03-14 23:29:27 -0300

    Posta a partir da base de dados

--- bsky.py
+++ bsky.py
@@ -1,6 +1,24 @@
 import json
+import random
 
 from atproto import Client
+
+MAX_LENGHT = 300
+ELLIPSIS = " (...)"
+
+with open('database.json') as f:
+    quotes = json.load(f)
+
+author, quote = random.choice(quotes['database']).values()
+
+footer = f'\n\n    -- {author}'
+quote_lenght = MAX_LENGHT - len(footer)
+
+if(len(quote) > quote_lenght):
+    quote_lenght -= len(ELLIPSIS)
+    quote = quote[:quote_lenght] + ELLIPSIS
+
+message = f'{quote}{footer}'
 
 with open('secrets.json') as f:
     credentials = json.load(f)
@@ -9,4 +27,4 @@ user, password = credentials.values()
 
 client = Client()
 client.login(user, password)
-post = client.send_post('Hello world! I posted this via the Python SDK.')
+post = client.send_post(message)
